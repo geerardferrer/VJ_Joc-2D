17933c51dc00746df7fdb49f49c2e3e089df7444
gerard.ferrer.oriol <gerard.ferrer.oriol@stark.fib.upc.edu>
2024-10-10 17:47:10 +0200

    Animacions

--- 2DGame/02-Bubble/02-Bubble/Player.cpp
+++ 2DGame/02-Bubble/02-Bubble/Player.cpp
@@ -12,7 +12,17 @@
 
 enum PlayerAnims
 {
-	STAND_LEFT, STAND_RIGHT, MOVE_LEFT, MOVE_RIGHT
+	STAND_LEFT, STAND_RIGHT, //FET
+	MOVE_LEFT, MOVE_RIGHT,  //FET
+	CROUCH_LEFT, CROUCH_RIGHT, //FET (FALTA MIRAR QUE NOMÉS S'HA D'AJUPIR QUAN ESTÀ AL TERRA)
+	JUMP_LEFT, JUMP_RIGHT, 
+	FALL_ASS_LEFT, FALL_ASS_RIGHT, 
+	DRIFT_LEFT, DRIFT_RIGHT, 
+	STAND_OBJ_LEFT, STAND_OBJ_RIGHT, 
+	MOVE_OBJ_LEFT, MOVE_OBJ_RIGHT, 
+	JUMP_OBJ_LEFT, JUMP_OBJ_RIGHT, 
+	CLIMB, 
+	VICTORY_STAND
 };
 
 
@@ -21,8 +31,9 @@ void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 	bJumping = false;
 
 	spritesheet.loadFromFile("images/Luigi_Sprites.png", TEXTURE_PIXEL_FORMAT_RGBA);
-	sprite = Sprite::createSprite(glm::ivec2(64, 64), glm::vec2(0.125f, 0.25f), &spritesheet, &shaderProgram);
-	sprite->setNumberAnimations(4);
+	sprite = Sprite::createSprite(glm::ivec2(96, 96), glm::vec2(0.125f, 0.25f), &spritesheet, &shaderProgram);
+	spritesheet.setMagFilter(GL_NEAREST);
+	sprite->setNumberAnimations(20);
 
 	sprite->setAnimationSpeed(STAND_LEFT, 8);
 	sprite->addKeyframe(STAND_LEFT, glm::vec2(0.125f, 0.f));
@@ -40,12 +51,20 @@ void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.5f, 0.f));
 	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.625f, 0.f));
 
+	sprite->setAnimationSpeed(CROUCH_LEFT, 8);
+	sprite->addKeyframe(CROUCH_LEFT, glm::vec2(0.f, 0.75f));
+
+	sprite->setAnimationSpeed(CROUCH_RIGHT, 8);
+	sprite->addKeyframe(CROUCH_RIGHT, glm::vec2(0.125f, 0.75f));
+
+
+
 	sprite->changeAnimation(0);
 	tileMapDispl = tileMapPos;
 	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
 
-	posCollision = glm::ivec2(16, 0);
-	sizeCollision = glm::ivec2(32, 64);
+	posCollision = glm::ivec2(33, 0);
+	sizeCollision = glm::ivec2(30, 96);
 }
 
 void Player::update(int deltaTime)
@@ -55,10 +74,10 @@ void Player::update(int deltaTime)
 	{
 		if (sprite->animation() != MOVE_LEFT)
 			sprite->changeAnimation(MOVE_LEFT);
-		posPlayer.x -= 6;
+		posPlayer.x -= 4;
 		if (map->collisionMoveLeft(posPlayer + posCollision, sizeCollision))
 		{
-			posPlayer.x += 6;
+			posPlayer.x += 4;
 			sprite->changeAnimation(STAND_LEFT);
 		}
 	}
@@ -66,18 +85,29 @@ void Player::update(int deltaTime)
 	{
 		if (sprite->animation() != MOVE_RIGHT)
 			sprite->changeAnimation(MOVE_RIGHT);
-		posPlayer.x += 6;
+		posPlayer.x += 4;
 		if (map->collisionMoveRight(posPlayer + posCollision, sizeCollision))
 		{
-			posPlayer.x -= 6;
+			posPlayer.x -= 4;
 			sprite->changeAnimation(STAND_RIGHT);
+		}
+	}
+	else if (Game::instance().getKey(GLFW_KEY_DOWN))
+	{
+		if (sprite->animation() == STAND_LEFT || sprite->animation() == MOVE_LEFT) {
+			std::cout << "Ha entrat al LEFT" << endl;
+			sprite->changeAnimation(CROUCH_LEFT);
+		}
+		else if (sprite->animation() == STAND_RIGHT || sprite->animation() == MOVE_RIGHT) {
+			std::cout << "Ha entrat al RIGHT" << endl;
+			sprite->changeAnimation(CROUCH_RIGHT);
 		}
 	}
 	else
 	{
-		if (sprite->animation() == MOVE_LEFT)
+		if (sprite->animation() == MOVE_LEFT || sprite->animation() == CROUCH_LEFT)
 			sprite->changeAnimation(STAND_LEFT);
-		else if (sprite->animation() == MOVE_RIGHT)
+		else if (sprite->animation() == MOVE_RIGHT || sprite->animation() == CROUCH_RIGHT)
 			sprite->changeAnimation(STAND_RIGHT);
 	}
 
