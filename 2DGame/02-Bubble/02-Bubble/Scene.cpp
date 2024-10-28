157f594d890a585ee2dd5b8235e4aaa28a65afb6
morsoth <morsoth119@gmail.com>
2024-10-28 18:22:25 +0100

    main menu sprite

--- 2DGame/02-Bubble/02-Bubble/Scene.cpp
+++ 2DGame/02-Bubble/02-Bubble/Scene.cpp
@@ -8,8 +8,8 @@
 #define SCREEN_X 0
 #define SCREEN_Y 0
 
-#define INIT_PLAYER_X_TILES 4	//3
-#define INIT_PLAYER_Y_TILES 70	//25
+#define INIT_PLAYER_X_TILES 3//4	//3
+#define INIT_PLAYER_Y_TILES 25//70	//25
 
 Scene::Scene()
 {
@@ -31,7 +31,7 @@ void Scene::init()
 	initShaders();
 
 	// TILEMAP
-	map = TileMap::createTileMap("levels/level01.txt", glm::vec2(SCREEN_X, SCREEN_Y), texProgram);
+	map = TileMap::createTileMap("levels/practice.txt", glm::vec2(SCREEN_X, SCREEN_Y), texProgram);
 
 	// PLAYER
 	player = new Player();
